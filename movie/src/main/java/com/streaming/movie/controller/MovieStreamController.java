cd2f11950aed52e199407255218cbbe3eb9df4a2
OG-Oluoch <makopiyo45@gmail.com>
2026-05-28 11:05:15 +0300

    added movie streaming module

---
+++ movie/src/main/java/com/streaming/movie/controller/MovieStreamController.java
@@ -0,0 +1,37 @@
+package com.streaming.movie.controller;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+@RestController
+public class MovieStreamController {
+
+    public static final String VIDEO_FILE = "C:\\Users\\Admin\\Downloads\\auk.mp4";
+
+    @GetMapping("/stream")
+    public ResponseEntity<InputStreamResource> streamMovie(String videoPath) throws FileNotFoundException {
+
+        File videoFile = new File(videoPath);
+        if(videoFile.exists()){
+
+            InputStreamResource streamResource = new InputStreamResource(new FileInputStream(videoFile));
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType("video/mp4"))
+                    .body(streamResource);
+
+        }
+        else {
+
+          return  ResponseEntity.notFound().build();
+        }
+
+
+    }
+}
---
+++ movie/src/test/java/com/streaming/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
